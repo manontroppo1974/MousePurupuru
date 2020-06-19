dfc80b39283153a7367aa6992501b59535420c73
manontroppo1974 <ma.non.troppo.1974@gmail.com>
2020-06-19 09:55:46 +0900

    first commit

---
+++ MousePurupuru/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace MousePurupuru
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkBox1.Location = new System.Drawing.Point(0, 0);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(122, 30);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "ON";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(122, 30);
+            this.Controls.Add(this.checkBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "まうすぷるぷる";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ MousePurupuru/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace MousePurupuru
+{
+    public partial class Form1 : Form
+    {
+        TimerCallback timerDelegate = null;
+        System.Threading.Timer timer = null;
+        const int timerPeriod = 30000;
+        const int timerDue = 30000;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timerDelegate = new TimerCallback(timer_Tick);
+            timer = new System.Threading.Timer(timerDelegate, null, System.Threading.Timeout.Infinite, timerPeriod);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+                timer.Change(timerDue, timerPeriod);
+            }
+            else
+            {
+                timer.Change(System.Threading.Timeout.Infinite, timerPeriod);
+            }
+        }
+
+        public void timer_Tick(object sender)
+        {
+            PreventScreenSaverFromStarting();
+        }
+
+        void PreventScreenSaverFromStarting()
+        {
+            INPUT input = new INPUT();
+            input.type = INPUT_MOUSE;
+            input.mi = new MOUSEINPUT();
+
+            input.mi.dwExtraInfo = IntPtr.Zero;
+            input.mi.dx = 0;
+            input.mi.dy = 0;
+            input.mi.time = 0;
+            input.mi.mouseData = 0;
+            input.mi.dwFlags = 0x0001; // MOVE (RELATIVE)
+            int cbSize = Marshal.SizeOf(typeof(INPUT));
+            uint r = SendInput(1, ref input, cbSize);
+        }
+
+        #region Win32 API
+        [StructLayout(LayoutKind.Sequential)]
+        struct MOUSEINPUT
+        {
+            public int dx;
+            public int dy;
+            public uint mouseData;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+        const int INPUT_MOUSE = 0;
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct KEYBDINPUT
+        {
+            ushort wVk;
+            ushort wScan;
+            uint dwFlags;
+            uint time;
+            IntPtr dwExtraInfo;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct HARDWAREINPUT
+        {
+            uint uMsg;
+            ushort wParamL;
+            ushort wParamH;
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        struct INPUT
+        {
+            [FieldOffset(0)]
+            public int type;
+            [FieldOffset(4)] //*
+            public MOUSEINPUT mi;
+            [FieldOffset(4)] //*
+            public KEYBDINPUT ki;
+            [FieldOffset(4)] //*
+            public HARDWAREINPUT hi;
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint SendInput(uint nInputs, ref INPUT pInputs, int cbSize);
+
+        [FlagsAttribute]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_SYSTEM_REQUIRED = 0x00000001,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            // Legacy flag, should not be used.
+            // ES_USER_PRESENT   = 0x00000004,
+            ES_CONTINUOUS = 0x80000000,
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+        #endregion
+    }
+}
